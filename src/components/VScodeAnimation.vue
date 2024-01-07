8540b31ca2f29cbd1b03c503311928969fc282f4
ROTO0504 <0504hiroosa@gmail.com>
2024-01-08 01:53:04 +0900

    lottie追加

---
+++ src/components/VScodeAnimation.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <section v-if="show" class="container">
+      <div>
+        <lottie :options="defaultOptions" v-on:animCreated="handleAnimation" />
+      </div>
+    </section>
+  </v-app>
+</template>
+
+<script>
+import Lottie from "~/components/Lottie.vue";
+import * as animationData from "~/assets/animation/VScode.json";
+
+export default {
+  components: {
+    Lottie,
+  },
+  data() {
+    return {
+      defaultOptions: {
+        animationData: animationData,
+      },
+      animationSpeed: 1,
+      show: true,
+    };
+  },
+  methods: {
+    handleAnimation: function (anim) {
+      this.anim = anim;
+    },
+  },
+};
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,113 +1,25 @@
-<script setup lang="ts">
-import type { Blog } from "~~/types/blog";
-
-const { data } = await useMicroCMSGetList<Blog>({
-  endpoint: "blogs",
-});
-const isLoading = ref(true);
-
-onMounted(() => (isLoading.value = false));
-</script>
+<script></script>
 
 <template>
-  <v-container>
-    <v-row class="pt-16" align="center" justify="center">
-      <v-col
-        v-for="blog in data?.contents"
-        :key="blog.id"
-        cols="12"
-        xl="4"
-        lg="6"
-        md="12"
-      >
-        <v-card class="rounded-lg hover fadeIn" elevation="24"
-          ><NuxtLink :to="`/${blog.id}`">
-            <v-carousel
-              class="pa-4"
-              hide-delimiters
-              cycle
-              show-arrows="hover"
-              height="100%"
-              width="100%"
-            >
-              <v-carousel-item
-                class="rounded-lg"
-                :show-arrows="false"
-                :src="blog.eyecatch?.url"
-                height="100%"
-                width="100%"
-              ></v-carousel-item>
-            </v-carousel>
-            <v-list two-line>
-              <v-list-item>
-                <v-list-item-title>{{ blog.title }}</v-list-item-title>
-                <v-list-item-subtitle>{{
-                  blog.category?.name
-                }}</v-list-item-subtitle>
-              </v-list-item>
-            </v-list>
-          </NuxtLink>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <h2 class="pt-10 center">工事中</h2>
+  <VScodeAnimation class="VScode" width="100px"></VScodeAnimation>
 </template>
 
-<style scoped lang="scss">
-main {
-  max-width: 1600px;
-  margin: 0 auto;
-
-  @media screen and (max-width: 860px) {
-    padding: 0 20px;
-  }
+<style scoped>
+.center {
+  text-align: center;
+  font-family: "M PLUS 1p", sans-serif;
+  font-style: bold;
+  font-weight: 800;
+  font-size: 60px;
 }
 
-.fadeIn {
-  animation-name: fadeInAnime;
-  animation-duration: 3s;
-  animation-fill-mode: forwards;
-  opacity: 0;
-}
-
-@keyframes fadeInAnime {
-  0% {
-    opacity: 0;
-  }
-
-  100% {
-    opacity: 1;
-  }
-}
-
-.upIn {
-  animation-name: upIn;
-  animation-duration: 3s;
-  animation-fill-mode: forwards;
-}
-
-@keyframes anim {
-  0% {
-    transform: translateY(300px);
-  }
-
-  100% {
-    transform: translateY(0px);
-  }
-}
-
-.hover {
-  transition: 0.3s;
-  &:hover {
-    transform: translateY(-10px);
-    backdrop-filter: blur(3px);
-  }
-}
-
-.hover_title {
-  transition: 0.3s;
-  &:hover {
-    color: #ff4081;
-  }
+.VScode {
+  width: 100%;
+  height: 100%;
+  min-height: 300px;
+  position: relative;
+  padding-top: 3%;
+  background-color: rgb(255, 255, 255);
 }
 </style>
