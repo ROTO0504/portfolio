2c45e14210299895bf2a15eac4bb987f91fc6df4
ROTO <0504hiroosa@gmail.com>
2024-04-16 14:06:39 +0900

    TitleBar復活

---
+++ src/components/TitleBar.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+defineProps({
+  titleBar: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h2 class="title">
+      {{ titleBar }}
+    </h2>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  color: #05091e;
+  min-width: 10vw;
+  font-size: 10vh;
+  text-align: center;
+}
+</style>
